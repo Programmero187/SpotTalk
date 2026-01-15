694f65ecc67eb368c0e85cef52a20bc63d3799b7
Pero <perokosic420@googlemail.com>
2026-01-15 02:28:30 +0100

    created index.faiss

--- socket_send.py
+++ socket_send.py
@@ -1,5 +1,3 @@
-
-"""
 import socket
 
 SOCKET_IP = "localhost"
@@ -13,15 +11,4 @@ while True:
     client, addr = server.accept()
     print(f"Connection from {addr} has been established!")
 
-"""
 
-
-import json
-
-with open("socket_temp/robot_response.json", "r") as json_file:
-    x = json.load(json_file)
-print(x.get("text"))
-x["text"] = "test"
-
-with open("socket_temp/robot_response.json", "w") as json_file:
-    json.dump(x, json_file, indent=4)
